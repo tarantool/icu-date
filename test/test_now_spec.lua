fe6d79a20194181c435e8c3da5d83f7e973933e0
Oleg Babin <babinoleg@mail.ru>
2019-03-01 07:23:49 +0300

    Extend icu-date API:
    * Add now() function to get current date and time since epoch
    * Add constants for months
    * Add constants for days of week

---
+++ test/test_now_spec.lua
@@ -0,0 +1,16 @@
+#!/usr/bin/env tarantool
+require('strict').on()
+
+local icu_date = require('icu-date')
+local tap = require('tap')
+
+local test = tap.test("now")
+test:plan(1)
+
+test:test("get current time", function(test)
+              test:plan(1)
+              local now = icu_date.now()
+              test:is(type(now), 'number')
+end)
+
+return test:check()
